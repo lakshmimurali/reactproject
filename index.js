a91d1435a9d746f6ac5d420c890db7e54ae74c22
lakshmimurali <vmanikandan@zohocorp.com>
2017-05-12 15:28:54 +0530

    Debug file
    Flle used for my debugging purpose.

---
+++ index.js
@@ -0,0 +1,19 @@
+function debug( message )
+{
+  console.debug( message );
+}
+
+function log( message )
+{
+  console.log( message );
+}
+
+function warn( message )
+{
+  console.warn( message );
+}
+
+function error( message )
+{
+  console.error( message );
+}
